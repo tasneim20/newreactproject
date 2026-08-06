e136139314d1d00ddd7a517697cb4b87cc8763c0
tasneim20 <tasneimkhlifat20@gmail.com>
2026-08-06 11:20:05 +0300

    centralize default axios headers language
    Co-Authored-By: Aseel Aburumman <169008634+Aseel-Aburumman@users.noreply.github.com>

--- client/src/features/auth/api/authApi.js
+++ client/src/features/auth/api/authApi.js
@@ -1,11 +1,7 @@
 import api from "@/lib/axios";
 
 export const login = async (data) => {
-  const response = await api.post("/authentications/login", data, {
-    headers: {
-      "Accept-Language": "EN",
-    },
-  });
+  const response = await api.post("/authentications/login", data);
 
   return response.data;
 };
